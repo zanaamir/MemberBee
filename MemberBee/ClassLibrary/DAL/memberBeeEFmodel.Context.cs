5cf1aa673d07b1d4c1d882c7592e7449debd6047
zanaamir <amirzana123@gmail.com>
2018-07-26 10:47:25 +0300

    jjjjjjjjjjj

--- MemberBee/ClassLibrary/DAL/memberBeeEFmodel.Context.cs
+++ MemberBee/ClassLibrary/DAL/memberBeeEFmodel.Context.cs
@@ -18,6 +18,7 @@ namespace ClassLibrary.DAL
         public memberBeeEntitiesDB()
             : base("name=memberBeeEntitiesDB")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
