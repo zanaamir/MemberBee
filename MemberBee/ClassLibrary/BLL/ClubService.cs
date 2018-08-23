195ee962953652f51927e8876d9011feace9858b
zanaamir <amirzana123@gmail.com>
2018-08-23 18:48:38 +0300

    tfgf

---
+++ MemberBee/ClassLibrary/BLL/ClubService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.DAL;
+
+namespace ClassLibrary.BLL
+{
+   public class ClubService
+    {
+        private igroup36_prodEntities db;
+
+        public ClubService(igroup36_prodEntities _db)
+        {
+            db = _db;
+        }
+
+        public Club GetClub(int _clubId)
+        {
+            return db.Clubs.SingleOrDefault(x => x.ClubId == _clubId);
+        }
+    }
+}
---
+++ MemberBee/ClassLibrary/BLL/CustomerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.DAL;
+
+namespace ClassLibrary.BLL
+{
+   public class CustomerService
+    {
+        
+        private igroup36_prodEntities db;
+        //בנאי להבאת DB 
+        public CustomerService(igroup36_prodEntities _db)
+        {
+            db = _db;
+        }
+
+        public Customer GetCustomer(string _email)
+        {
+            return db.Customers.SingleOrDefault(x => x.MainMail == _email);
+        }
+    }
+}
