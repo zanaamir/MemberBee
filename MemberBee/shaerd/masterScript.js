c188fbcdf643e40b7b33737dae3fc0458a261db4
adva1 <advakolski@gmail.com>
2018-10-07 10:47:35 +0300

    Add project files.

---
+++ MemberBee/shaerd/masterScript.js
@@ -0,0 +1,49 @@
+﻿function AjaxJsonHandler(_url , _type,_data , success_callback, error_callback) {
+
+ 
+    $.ajax({
+        datatype: "json",
+        url: _url,
+        contentType: "application/json; charset=utf-8",
+        method: _type,
+        data: JSON.stringify(_data),
+        success: success_callback,
+        error: error_callback,
+    });
+}
+
+function GenericError(err){
+    console.log(err)
+   
+    
+}
+function AlredyExist(err) {
+    alert("הלקוח כבר רשום למועדון זה");
+}
+
+
+var systemHelper = {
+    webApiEndPoints: {
+        login: "/api/LOGIN",
+        club: "/api/club/",
+        customer : "/api/club",
+        AddCustomerToClub: "/api/AddCustomerToClub/",
+
+    },
+    HttpVerbs:{
+        POST: "POST",
+        GET : "GET",
+        DELETE : "DELETE",
+        PUT : "PUT",
+
+    
+    },
+    storege:{
+        user: "user",
+        club : "club",
+        // changed from clubId : "clubId"
+    },
+    getFromStorege: function (key) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+}
